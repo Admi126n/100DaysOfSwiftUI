2e2c51ac17a7cde2a16fe7ca68600c103cbdc0f1
Adam Tokarski <a.tokarski126@gmail.com>
2023-08-25 11:07:05 +0200

    17 day compleated

---
+++ Projects/WeSplit/README.md
@@ -0,0 +1,8 @@
+# WeSplit
+
+In this project we’re going to be building a check-splitting app that you might use after eating at a restaurant – you enter the cost of your food, select how much of a tip you want to leave, and how many people you’re with, and it will tell you how much each person needs to pay.
+
+#### Project pages
+- https://www.hackingwithswift.com/100/swiftui/16
+- https://www.hackingwithswift.com/100/swiftui/17
+- https://www.hackingwithswift.com/100/swiftui/18
--- Projects/WeSplit/WeSplit/ContentView.swift
+++ Projects/WeSplit/WeSplit/ContentView.swift
@@ -8,43 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
-    @State private var selectedName = "Adam"
-    private let names = ["Adam", "Ida", "Paul"]
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    private let peopleRange = 2...10
+    private let tipPercentages = [0, 5, 10, 15, 20, 25]
+    private var currencyCode = Locale.current.currency?.identifier ?? "PLN"
+    
+    private var amountPerPerson: Double {
+        (checkAmount * (1.0 + (Double(tipPercentage) / 100.0)) / Double(numberOfPeople))
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    ForEach(names, id: \.self) {
-                        Text("Hello \($0)!")
+                    TextField("Amount", value: $checkAmount, format: .currency(code: currencyCode))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Stepper("Number of people: \(numberOfPeople)",value: $numberOfPeople,
+                            in: peopleRange)
+                }
+                
+                Section {
+                    Picker("Tip percantage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
                     }
+                    .pickerStyle(.segmented)
                 } header: {
-                    Text("Hello section")
+                    Text("Tip percentage")
                         .textCase(nil)
                 }
                 
                 Section {
-                    Button("Tap count: \(tapCount)") {
-                        tapCount += 1
-                    }
-                    TextField("Enter your name", text: $name)
-                    Picker("Choose your name", selection: $selectedName) {
-                        ForEach(names, id: \.self) {
-                            Text($0)
-                        }
-                    }
-                    
+                    Text(amountPerPerson, format: .currency(code: currencyCode))
                 } header: {
-                    Text("Other")
+                    Text("Amount per person")
                         .textCase(nil)
                 }
-
             }
-            .navigationTitle("SwiftUI")
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        
     }
 }
 
